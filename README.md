084bbd2b60ecf71d5b08cdf611db84446eb95d14
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-09-05 15:04:23 +0700

    Fith Uploads

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BC29_REACTJS-BTCAPSTONE_NHOM-8
+
+Tiến độ (30%): 
+	+ Lê Thành Đạt:  Làm hết các chức năng của trang 
--- capston-reactjs-nhom8/src/components/footer/footer.jsx
+++ capston-reactjs-nhom8/src/components/footer/footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="footer bg-light py-5 text-center">Designed by Thanh Dat Le & Dieu Hang</div>
+    <div className="footer bg-light py-5 text-center">Designed by Thanh Dat Le</div>
   )
 }
